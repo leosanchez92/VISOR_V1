be73a0995f2705d790a5907ba33c9e61b4905d0e
leosanchez92 <nelsonsanchez.al@gmail.com>
2022-05-30 13:43:20 -0400

    Initial commit

---
+++ 5_SALUD/data/postas_rural.js
@@ -0,0 +1,19 @@
+var postasImperial = {
+"type": "FeatureCollection",
+"name": "postas_rural",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "Molco", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -73.111934089363942, -38.860002991671543 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Mañio", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -73.044804875647586, -38.850739855601873 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Alto Boroa", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -72.834801687568614, -38.883484299988709 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Bochoco", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -72.949018645727065, -38.823772234278223 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Chivilcoyan", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -72.967216261964666, -38.633508164998368 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Miramar", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -73.076755588120506, -38.599029680981609 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Rulo", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -73.025499539856469, -38.756646507283016 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Pidenko", "TIPO": "Estación Medico Rural" }, "geometry": { "type": "Point", "coordinates": [ -73.06556893042206, -38.689700789264947 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Puente Negro", "TIPO": "Estación Medico Rural" }, "geometry": { "type": "Point", "coordinates": [ -73.047098796050733, -38.80476546604342 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Entre Rios", "TIPO": "Estación Medico Rural" }, "geometry": { "type": "Point", "coordinates": [ -72.849348879194707, -38.785947054091096 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Filulawen", "TIPO": "Intercultural" }, "geometry": { "type": "Point", "coordinates": [ -72.889291747666476, -38.837574967217314 ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "Queupúe", "TIPO": "Posta" }, "geometry": { "type": "Point", "coordinates": [ -72.87315111851089, -38.904858770168119 ] } }
+]
+}
